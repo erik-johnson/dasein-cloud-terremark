3155b8497341db5ff73146a285b4734f321e894b
Erik Johnson <erik.johnson@enstratus.com>
2013-08-28 16:36:08 -0500

    [bugzid: 2862] Missing Content-Length header in some calls.

--- src/main/java/org/dasein/cloud/terremark/Terremark.java
+++ src/main/java/org/dasein/cloud/terremark/Terremark.java
@@ -355,6 +355,7 @@ public class Terremark  extends AbstractCloud {
 		headers.put(ACCEPT, XML);
 		if (body != null && (methodType.equals(HttpMethodName.PUT) || methodType.equals(HttpMethodName.POST) || methodType.equals(HttpMethodName.DELETE))){
 			contentLength = new String("" + body.getBytes().length);
+            headers.put(CONTENT_LENGTH, contentLength);
 		}
 		if (body != null && !body.equals("")){
 			contentType = XML;
